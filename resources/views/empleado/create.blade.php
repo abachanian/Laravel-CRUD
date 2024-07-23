2ba93d291346222f2cc37737faee4d7fe6b25665
Alejandro <abachanian@gmail.com>
2024-07-23 09:12:27 -0300

    ok

---
+++ resources/views/empleado/create.blade.php
@@ -0,0 +1,21 @@
+Formulario de creaión de empleado
+
+<form action="" method="post" enctype="multipart/form-data">
+
+    <label for="Nombre">Nombre</label>
+    <input type="text" name="Nombre" id="Nombre"><br>
+    
+    <label for="Apellido">Apellido</label>
+    <input type="text" name="Apellido" id="Apellido"><br>
+    
+    <label for="Nro_documento">N°documento</label>
+    <input type="text" name="Nro_documento" id="Nro_documento"><br>
+    
+    <label for="Correo">Correo</label>
+    <input type="text" name="Correo" id="Correo"><br>
+
+    <label for="Foto">Foto</label>
+    <input type="file" name="Foto" id="Foto"><br>
+    <input type="submit" name="Enviar" id="Enviar">
+
+</form>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EmpleadoController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+/* Forma fácil
+Route::get('/empleado', function () {
+    return view('empleado.index');
+});
+
+a través del controller
+Route::get('/empleado/create',[EmpleadoController::class,'create']);*/
+
+Route::resource('empleado',EmpleadoController::class);
